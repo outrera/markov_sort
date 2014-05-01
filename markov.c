59914d643ed77800981a4a9aa82160466191fd3d
saniv <snv985@gmail.com>
2014-05-01 17:21:46 +0400

    initial commit

---
+++ markov.c
@@ -0,0 +1,156 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <math.h>
+#include <time.h>
+
+#define SIZE 16
+#define NCHAINS 1000
+#define BITS 8
+#define TRNS (1<<BITS)
+
+typedef struct chain {
+  int Ls[BITS]; // bit locations in 32-element array
+  uint16_t Ss[TRNS][TRNS]; // statistics
+  uint16_t Ts[TRNS]; // transitions
+} chain;
+
+chain Cs[NCHAINS];
+uint8_t Is[SIZE]; // input state
+uint8_t Os[SIZE]; // output state
+int Vs[SIZE][2]; // vote space
+
+int bitCount(uint32_t v) {
+  uint32_t c = v - ((v >> 1) & 0x55555555);
+  c = ((c >> 2) & 0x33333333) + (c & 0x33333333);
+  c = ((c >> 4) + c) & 0x0F0F0F0F;
+  c = ((c >> 8) + c) & 0x00FF00FF;
+  c = ((c >> 16) + c) & 0x0000FFFF;
+  return (int)c;
+}
+
+int cmp(const void *a, const void *b) {
+  return (int)*(uint8_t*)a - (int)*(uint8_t*)b;
+}
+
+void exampleSort() {
+  memcpy(Os, Is, SIZE);
+  heapsort(Os, SIZE, 1, cmp);
+}
+
+int rnd(int N) {
+  return round((double)rand()/RAND_MAX*(N-1));
+}
+
+void randomizeInput() {
+  int I;
+  for (I = 0; I < SIZE; I++) Is[I] = (uint8_t)rnd(256);
+}
+
+int getBit(uint8_t *Bs, int L) {
+  return rnd(256)<Bs[L] ? 1 : 0;
+}
+
+uint8_t gatherBits(uint8_t *Bs, int *Ls) {
+  int I;
+  uint8_t R = 0;
+  for (I = 0; I < BITS; I++) R |= getBit(Bs,Ls[I]) << I;
+  return R;
+}
+
+void chainInit(chain *C) {
+  int I;
+  for (I = 0; I < BITS; I++) C->Ls[I] = rnd(SIZE);
+}
+
+void initChains() {
+  int I;
+  for (I = 0; I < NCHAINS; I++) chainInit(Cs+I);
+}
+
+void chainTrain(chain *C) {
+  uint8_t In = gatherBits(Is, C->Ls);
+  uint8_t Out = gatherBits(Os, C->Ls);
+  C->Ss[In][Out]++;
+}
+
+void chainFinishTraining(chain *C) {
+  int I, J;
+  for (I = 0; I < TRNS; I++) {
+    int best_count = 0;
+    int best_transition = 0;
+    for (J = 0; J < TRNS; J++) {
+      if (C->Ss[I][J] > best_count) {
+        best_count = C->Ss[I][J];
+        best_transition = J;
+      }
+    }
+    C->Ts[I] = best_transition;
+  }
+}
+
+void trainChains(int Examples) {
+  int I;
+  while (Examples-- > 0) {
+    switch (rnd(4)) { //degenerate cases
+    case 0: memset(Is, 0, SIZE); break;
+    case 1: memset(Is, 0xFF, SIZE); break;
+    default: randomizeInput();
+    }
+    exampleSort();
+    for (I = 0; I < NCHAINS; I++) chainTrain(Cs+I);
+  }
+  for (I = 0; I < NCHAINS; I++) chainFinishTraining(Cs+I);
+}
+
+void chainVote(chain *C) {
+  int I;
+  uint8_t In = gatherBits(Is, C->Ls);
+  uint8_t Out = C->Ts[In];
+  for (I = 0; I < BITS; I++) {
+    Vs[C->Ls[I]][0] += (Out&(1<<I)) ? 1 : 0;
+    Vs[C->Ls[I]][1]++;
+  }
+}
+
+void markovSort() {
+  int I;
+  memset(Vs, 0, SIZE*2*sizeof(int));
+  memset(Os, 0, SIZE);
+  for (I = 0; I < NCHAINS; I++) chainVote(Cs+I);
+  for (I = 0; I < SIZE; I++) {
+    Os[I] = (uint8_t)round((double)Vs[I][0]/Vs[I][1]*255);
+  }
+}
+
+void printArray(char *Info, uint8_t *Xs) {
+  printf("%s", Info);
+  int I;
+  for (I = 0; I < SIZE; I++) {
+    if (I != 0) printf(",");
+    printf("%03d", Xs[I]);
+  }
+  printf("\n");
+}
+
+int main() {
+  srand((unsigned)time(0));
+
+  printf("Training...\n");
+  initChains();
+  trainChains(10000);
+
+  randomizeInput();
+  //memset(Is, 0, SIZE);
+  //memset(Is, 255, SIZE);
+  printArray("Sorting:", Is);
+
+  markovSort();
+  printArray("Guess:  ", Os);
+
+  exampleSort();
+  printArray("Answer: ", Os);
+  return 0;
+}
+
